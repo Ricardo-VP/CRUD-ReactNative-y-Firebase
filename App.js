0dc888497d376cee8b77a1da7db22bb1d9676e2c
RICARDO VACA <ricardo.vaca@pucese.edu.ec>
2020-11-14 16:36:35 -0500

    Instalación React Navigation

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack'
+
+const Stack = createStackNavigator()
+
+function myStack(){
+  return(
+    <Stack.Navigator>
+      <Stack.Screen/>
+    </Stack.Navigator>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hola mundo</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#090979',
+    backgroundColor: "#ccc",
     alignItems: 'center',
     justifyContent: 'center',
   },
